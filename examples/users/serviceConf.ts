279a73ccbac9fcf958b64e095df98d7e91b086b7
Ahmad Ameen <ahmadmameen7@gmail.com>
2024-01-22 19:27:55 +0100

    update example service config

--- examples/users/serviceConf.ts
+++ examples/users/serviceConf.ts
@@ -13,12 +13,4 @@ export class UserAPI extends BaseAPI {
     "Authorization": `Bearer ${this.token}`,
   };
   errorExceptionClass = ApiException;
-
-  async search (searchObj: SearchObj) {
-    let requestQuery = "?active=true";
-    Object.keys(searchObj).forEach((key: string) => {
-      requestQuery += `&${key}=${searchObj[key]}`;
-    });
-    return await this.get(requestQuery);
-  }
 }
